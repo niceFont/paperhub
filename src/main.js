11c71dcdbea9bda9e6565067e020bf02dfb4e85f
nicefont <peterwilms@hotmail.de>
2020-03-15 13:45:01 +0100

    initial commit

--- src/main.js
+++ src/main.js
@@ -2,11 +2,23 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
+
+Vue.prototype.$http = async (url, options) => {
+  fetch(url,
+    {
+      ...options,
+      headers: {
+        'api-key': 'xyz',
+      },
+    });
+};
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount('#app');
